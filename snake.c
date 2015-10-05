2db3589d66b93813a02ccee06d7587d8deeda297
Curton <liukedun@qq.com>
2015-10-06 00:16:06 +0800

    Add after game over wait time

--- snake.c
+++ snake.c
@@ -1,4 +1,3 @@
-
 //Funny-Snake Game
 //Code by Covey
 //2015/09/30 14:00  Movement controlled point       Version 1.00
@@ -7,20 +6,21 @@
 //2015/10/05 21:00  Optimize the program structure  Version 1.02
 //2015/10/05 22:16  Release version                 Version 2.0
 //2015/10/05 23:24  Optimize food appear time       Version 2.1
+//2015/10/06 00:02  Add after game over wait time   Version 2.11
 
 #include <stdio.h>
 #include <windows.h>
 #include <time.h>
 
-int snake_direction = 77;                   //snake start movement direction
-int *p_snake_direction = &snake_direction;   //the pointer of ...
+int snake_direction = 77;                   // snake start movement direction
+int *p_snake_direction = &snake_direction;  // the pointer of ...
 
 //Keyboard getch thread
 DWORD WINAPI ThreadProc1( LPVOID lpParam )
 {
     while(1)
     {
-        get_direction(p_snake_direction);   //Monitor keyboard input
+        get_direction(p_snake_direction);   // Monitor keyboard input
     }
 }
 void printscreen(int a[10][10], int t,int score)
@@ -76,6 +76,7 @@ int get_direction(int *pointer)
     }
     *pointer = key_2;
 }
+// print game over screen
 void game_over(int score)
 {
     int i = 0;
@@ -88,7 +89,9 @@ void game_over(int score)
     for(i=0;i<4;i++)printf("\n#                   #");
     printf("\n");
     for(i=0; i<21; i++)printf("#");
+    sleep(100);
 }
+// random create food
 void random_food(int *pointer)
 {
     int n = 0;
@@ -98,7 +101,7 @@ void random_food(int *pointer)
     int conflict = 1;
     do
     {
-        srand(GetTickCount());
+        srand(GetTickCount());// set random seed
         n = rand()%(0-10);
         i++;
         srand(GetTickCount()+i);
@@ -113,7 +116,7 @@ void random_food(int *pointer)
     while(conflict);
 
 }
-//Start
+// judge whether input negative direction
 int if_negative_direction(int snake_direction,int last_snake_direction,int *pointer)
 {
     int i = 0;
@@ -143,14 +146,15 @@ int if_negative_direction(int snake_direction,int last_snake_direction,int *poin
     }
     return i;
 }
+//Start game
 int main()
 {
-    int a[10][10] = {0};             //snake pixel point location
-    int *p_a = &a;                   //pointer of a
+    int a[10][10] = {0};             // snake pixel point location
+    int *p_a = &a;                   // pointer of a
     int last_snake_direction = snake_direction;
     int snake_length = 4;            //
-    int snake_head_x = 0;            //location of snake head of x
-    int snake_head_y = 3;            //location of snake head of y
+    int snake_head_x = 0;            // location of snake head of x
+    int snake_head_y = 3;            // location of snake head of y
     int food_location[10][10] = {0}; //
     int if_get_direction = 0;        //
     int latency = 400;               // default 400ms
@@ -165,28 +169,26 @@ int main()
     0,                               // use default creation flags
     NULL);                           // returns the thread identifier
 
-    //set snake head location
+                                     //set snake head location
     a[snake_head_x][snake_head_y]   = snake_length;
-    random_food(p_a);
-    //a[5][5] = -1;
-    //printf("%d",a[5][5]);
-    printscreen(a,0,score);
+    random_food(p_a);                // initialize snake food
+    printscreen(a,0,score);          // print first screen
     do
     {
 
         //IF INPUT NEGATIVE DIRECTION,KEEP LAST DIRECTION
         if(if_negative_direction(snake_direction,last_snake_direction,p_snake_direction))
         {
-            continue;
+            continue;                 //jump out following statement and run "do" again
         }
 
-        if(snake_direction == 0)     //IF INPUT ERROR,GET LAST MOVEMENT
+        if(snake_direction == 0)      //IF INPUT ERROR,GET LAST MOVEMENT
         {
             snake_direction = last_snake_direction;
         }
         else if(snake_direction == 77)//INPUT RIGHT
         {
-            if(snake_head_y != 9 && a[snake_head_x][snake_head_y+1] <= 0)//NOT IMPACT WITH THE EDGE
+            if(snake_head_y != 9 && a[snake_head_x][snake_head_y+1] <= 0)//NOT IMPACT WITH THE EDGE AND BODY
             {
 
                 int i = 0;
@@ -210,7 +212,7 @@ int main()
                 game_over(score);
                 break;
             }
-                       //Add food when food was eaten
+            //Add food when food was eaten
             {
                 int i = 0;
                 int j = 0;
@@ -236,7 +238,7 @@ int main()
         }
         else if(snake_direction == 80)//INPUT DOWN
         {
-            if(snake_head_x != 9 && a[snake_head_x+1][snake_head_y] <= 0)
+            if(snake_head_x != 9 && a[snake_head_x+1][snake_head_y] <= 0)//NOT IMPACT WITH THE EDGE AND BODY
             {
                 int i = 0;
                 int j = 0;
@@ -259,7 +261,7 @@ int main()
                 game_over(score);
                 break;
             }
-                       //Add food when food was eaten
+            //Add food when food was eaten
             {
                 int i = 0;
                 int j = 0;
@@ -285,7 +287,7 @@ int main()
         }
         else if(snake_direction == 72)//INPUT UP
         {
-            if(snake_head_x != 0 && a[snake_head_x-1][snake_head_y] <= 0)
+            if(snake_head_x != 0 && a[snake_head_x-1][snake_head_y] <= 0)//NOT IMPACT WITH THE EDGE AND BODY
             {
                 int i = 0;
                 int j = 0;
@@ -308,7 +310,7 @@ int main()
                 game_over(score);
                 break;
             }
-                       //Add food when food was eaten
+            //Add food when food was eaten
             {
                 int i = 0;
                 int j = 0;
@@ -334,7 +336,7 @@ int main()
         }
         else if(snake_direction == 75)//INPUT LEFT
         {
-            if(snake_head_y != 0 && a[snake_head_x][snake_head_y-1] <= 0)
+            if(snake_head_y != 0 && a[snake_head_x][snake_head_y-1] <= 0)//NOT IMPACT WITH THE EDGE AND BODY
             {
                 int i = 0;
                 int j = 0;
@@ -398,7 +400,7 @@ int main()
 
 
 /*
-// How to input value to a[][]
+// How record input value to a[][]
 int main()
 {
     int a[10][10] =
@@ -407,15 +409,14 @@ int main()
         {0,0,0,0,0,0,0,0,0,0},//0
         {0,0,0,0,0,0,0,0,0,0},//1
         {0,0,0,0,0,0,0,0,0,0},//2
-        {0,0,0,0,0,5,0,0,0,0},//3
-        {0,0,0,0,0,0,5,0,0,0},//4
-        {0,0,0,0,0,0,0,5,0,0},//5
-        {0,0,0,0,0,0,0,0,5,0},//6
-        {0,0,0,0,0,0,0,0,0,5},//7
+        {0,0,0,0,0,1,2,0,0,0},//3
+        {0,0,0,0,0,0,3,0,0,0},//4
+        {0,0,0,0,0,0,4,5,6,0},//5
+        {0,0,0,0,0,0,0,0,7,0},//6
+        {0,0,0,0,0,0,0,0,8,9},//7
         {0,0,0,0,0,0,0,0,0,0},//8
         {0,0,0,0,0,0,0,0,0,0},//9
     };
-    output_screen(a);
-    output_screen(a);
 }
 */
+
